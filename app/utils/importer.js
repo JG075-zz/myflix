a25922e19514e477237ab378b3d0ace66ab9d5ea
James Gardiner <jamesgardiner075@gmail.com>
2017-04-25 12:46:01 +0100

    Add importer outline

---
+++ app/utils/importer.js
@@ -0,0 +1,11 @@
+// npm import thisfile.json
+  // >> node ./app/utils/importer.js thisfile.json
+  // store before length
+
+console.log(process.argv[2]);
+// fs read process.argv[2] >> for each
+  // updatedMovie = infoFetcher.fetch(movie.title);
+  // >> var movie = new Movie { title: updatedMovie.title, updatedMovie.released}
+  // movie.save
+// get after length
+// console log complete + before length, after length
