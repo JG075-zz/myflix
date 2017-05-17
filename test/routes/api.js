9780fdc0de5af03366935c190a441740d6fccccd
James Gardiner <jamesgardiner075@gmail.com>
2017-05-17 14:32:19 +0100

    Fix api routes test

--- test/routes/api.js
+++ test/routes/api.js
@@ -1,33 +1,33 @@
-// var expect = require('chai').expect,
-//     sinon = require('sinon'),
-//     request = require('supertest'),
-//     app = require('../../app');
-//
-// describe('API Route', function() {
-//
-//   describe('/api', function(done) {
-//     it('should result in a 200 response', function() {
-//       request(app)
-//         .get('/api/')
-//         .expect(200, "ok")
-//         .end(function(err, res){
-//            if (err) throw err;
-//            done();
-//         });
-//     });
-//   });
-//
-//   describe('/api/movies', function() {
-//     it('should return a JSON object', function(done){
-//       request(app)
-//         .get('/api/movies')
-//         .expect(200, "ok")
-//         .expect('Content-Type', /json/)
-//         .end(function(err, res){
-//            if (err) throw err;
-//            done();
-//         });
-//     });
-//   });
-//
-// });
+var expect = require('chai').expect,
+    sinon = require('sinon'),
+    request = require('supertest'),
+    app = require('../../app');
+
+describe('API Route', function() {
+
+  describe('/api', function() {
+    it('should result in a 200 response', function(done) {
+      request(app)
+        .get('/api/')
+        .expect(200, "ok")
+        .end(function(err, res){
+           if (err) throw err;
+           done();
+        });
+    });
+  });
+
+  describe('/api/movies', function() {
+    it('should return a JSON object', function(done){
+      request(app)
+        .get('/api/movies')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res){
+           if (err) throw err;
+           done();
+        });
+    });
+  });
+
+});
