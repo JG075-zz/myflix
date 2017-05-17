eda5fb17a146f1beb2e7ea78639df7bdc75bfba6
James Gardiner <jamesgardiner075@gmail.com>
2017-05-17 14:30:49 +0100

    Modify controller to return movies

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -8,8 +8,8 @@ exports.movies = function(req, res) {
   // give back a json array of movie objects
   // should be limited to the first page e.g. first 20 results
   // should be sorted by release date (newest - oldest)
-  
-  Movie.find().sort('released').exec(function(error, movies) {
+
+  Movie.find().sort('released').limit(20).exec(function(error, movies) {
     if (error) {
       return res.send(400, {
         message: error
